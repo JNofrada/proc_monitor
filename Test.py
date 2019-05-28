928766a818c102bb11cb180a95b738535f5993f1
Jeremiah <jnnofrada@gmail.com>
2019-05-28 17:13:23 -0400

    cut to PID

--- Test.py
+++ Test.py
@@ -1,4 +1,10 @@
 import subprocess
 output = subprocess.Popen(['sysctl', '-n', 'hw.ncpu'], stdout=subprocess.PIPE).communicate()[0]
+newput = subprocess.Popen(['ps', '-A', '-o', '%cpu', '|', 'awk', "'{s+=1}", 'END', '{print', "s}'"], stdout=subprocess.PIPE).communicate()[0]
 print output,
+print newput,
+#int(output)
+#int(newput)
+#print (output)
+#print (newput)
 print ("cool")
--- test.sh
+++ test.sh
@@ -2,6 +2,10 @@ core=$(sysctl -n hw.ncpu)
 echo $core
 s=$(ps -A -o %cpu | awk '{s+=$1} END {print s}')
 per=$(printf "%.0f" $s)
+Echo $s
 echo $per
 cpu=$(($per/$core))
 echo $cpu
+NAME=$(whoami)
+echo $NAME
+top -F -R -o cpu -U $NAME -l 2 -n 2 -ncols 2 | grep PID -A 2 | cut -c "
